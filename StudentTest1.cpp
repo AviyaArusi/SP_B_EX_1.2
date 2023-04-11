43d5a74b414aefc47d6ac8ee033e3cc221bbbd15
AviyaArusi <110820987+AviyaArusi@users.noreply.github.com>
2023-04-11 07:22:53 +0000

    WIP

--- StudentTest1.cpp
+++ StudentTest1.cpp
@@ -36,6 +36,7 @@ TEST_CASE("The card scheme at the end of the game")
     Game game(p1, p2);
     game.playAll();
     game.printStats();//***********************************************************************!!!!!!!
+    cout << "$$$$$$$$$$$$$$$$$$##################################################"<< endl;
     int sum = p1.stacksize() + p1.cardesTaken() + p2.stacksize() + p2.cardesTaken();
     CHECK(sum == 52);
 }
